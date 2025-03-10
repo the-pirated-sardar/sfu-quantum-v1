c6c2c58d04c0e101d95dd8f911c111a18b4e255b
Prateek Singh Arora <psarora@sfu.ca>
2025-03-10 00:06:09 -0700

    Initial commit

---
+++ precache-manifest.698c311dcca16066896045587a03659b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04098f4133954697f554e02ffe71ba6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e80f8398495748a5d8e",
+    "url": "/static/css/main.854587b6.chunk.css"
+  },
+  {
+    "revision": "c21a928e9a2d97c3da03",
+    "url": "/static/js/2.35354ff2.chunk.js"
+  },
+  {
+    "revision": "d02c7d44ef30a6997d73dc3c8aee1dea",
+    "url": "/static/js/2.35354ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e80f8398495748a5d8e",
+    "url": "/static/js/main.ddfbabbe.chunk.js"
+  },
+  {
+    "revision": "5e57583ae65277e6653a",
+    "url": "/static/js/runtime-main.14c4daaa.js"
+  },
+  {
+    "revision": "cdaf4eb694166d61a58f476bb1926ac6",
+    "url": "/static/media/HealthcareIcon.cdaf4eb6.svg"
+  },
+  {
+    "revision": "77e4f8fc780608d8b84cca009ac721d0",
+    "url": "/static/media/agritech.77e4f8fc.svg"
+  },
+  {
+    "revision": "d72db2182f19e29c01875e9a61444170",
+    "url": "/static/media/calendar.d72db218.svg"
+  },
+  {
+    "revision": "cf28908a2727cd6fd3bf9b021ecad2ef",
+    "url": "/static/media/climate.cf28908a.svg"
+  },
+  {
+    "revision": "6df5f52da45532e6846da8462770f7b9",
+    "url": "/static/media/cyber.6df5f52d.svg"
+  },
+  {
+    "revision": "09ca94f39847e09347c1c4404fdb3394",
+    "url": "/static/media/fintech.09ca94f3.svg"
+  },
+  {
+    "revision": "97749b0f614fbd63c3ff5b30fe6e0a80",
+    "url": "/static/media/innovation.97749b0f.svg"
+  },
+  {
+    "revision": "ce8e4dd1bc1e994f7520275cbd434957",
+    "url": "/static/media/person.ce8e4dd1.svg"
+  },
+  {
+    "revision": "740cafdfd7b786a410a6ce9295c3855a",
+    "url": "/static/media/phone.740cafdf.svg"
+  },
+  {
+    "revision": "48dc51bf44cb30d4f538b315e2250562",
+    "url": "/static/media/women.48dc51bf.svg"
+  }
+]);
